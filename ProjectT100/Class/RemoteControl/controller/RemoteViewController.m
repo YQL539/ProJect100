ccef4ef6c5c45a2767baffe9fc967de76f24cbf6
yangqinglong <17712121207@163.com>
2017-09-25 22:47:57 +0800

    手势

--- ProjectT100/Class/RemoteControl/controller/RemoteViewController.m
+++ ProjectT100/Class/RemoteControl/controller/RemoteViewController.m
@@ -29,9 +29,14 @@
     _voiceView.backgroundColor = [UIColor yellowColor];
     [self.view addSubview:_voiceView];
     
+    UILongPressGestureRecognizer *press = [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(longPressAct:)];
+    [_voiceView addGestureRecognizer:press];
     
-    _timer = [NSTimer scheduledTimerWithTimeInterval:0.2f target:self selector:@selector(timerAction) userInfo:nil repeats:YES];
-    [[NSRunLoop mainRunLoop] addTimer:_timer forMode:NSRunLoopCommonModes];
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(panAct:)];
+    [_voiceView addGestureRecognizer:pan];
+    
+//    _timer = [NSTimer scheduledTimerWithTimeInterval:0.2f target:self selector:@selector(timerAction) userInfo:nil repeats:YES];
+//    [[NSRunLoop mainRunLoop] addTimer:_timer forMode:NSRunLoopCommonModes];
 }
 - (void)timerAction
 {
@@ -43,5 +48,36 @@
     }
 }
 
+//长按函数
+-(void)longPressAct:(UILongPressGestureRecognizer*)longpress{
+    //手势的状态对象，到达规定时间，3秒钟，触发函数
+    if(longpress.state==UIGestureRecognizerStateBegan){
+        _voiceView.progress += 0.01;
+        NSLog(@"状态开始");
+    }
+    //当手指离开屏幕时，结束状态
+    else if(longpress.state==UIGestureRecognizerStateEnded){
+        NSLog(@"结束状态");
+        
+    }
+    
+}
+
+//移动事件函数，只要手指坐标在屏幕上发生变化时，函数就会被调用
+-(void)panAct:(UIPanGestureRecognizer*)pan{
+    
+    //获取移动的坐标，现对于视图的坐标系
+    //参数：相对的视图对象
+    
+    //    CGPoint pt = [pan translationInView:self.view];
+    //    NSLog(@"pt.x=%.2f,pt.y=%.2f",pt.x,pt.y);
+    
+    //获取移动时的相对速度
+    _voiceView.progress += 0.01;
+    CGPoint pv = [pan velocityInView:self.view];
+    NSLog(@"pv.x=%.2f,pv.y=%.2f",pv.x,pv.y);
+    
+    
+}
 
 @end
